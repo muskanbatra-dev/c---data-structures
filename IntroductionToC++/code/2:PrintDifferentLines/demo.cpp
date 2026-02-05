b7fe95ad06b2ed063d74562d7d90000436a03c0e
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2026-02-05 22:16:20 +0530

    title: added

--- IntroductionToC++/code/2:PrintDifferentLines/demo.cpp
+++ IntroductionToC++/code/2:PrintDifferentLines/demo.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl << "Muskan Batra" << endl;
+    cout << "Hello World" << endl ;
+    cout << "100x school" << endl ;
+    cout << "Hello 3" << endl;
+    cout << "Hello 5" << endl;
+    
     
 }
